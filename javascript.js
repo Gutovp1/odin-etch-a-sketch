de598b29b9eddb2bc7fcc94d0d13750dbf4ab3f6
Gutovp1 <paivaav@gmail.com>
2022-05-02 16:26:17 -0300

    Crate the fixed dimensios pad with squares and the mouse track

--- javascript.js
+++ javascript.js
@@ -1,18 +1,30 @@
 const bod = document.querySelector('body');
 bod.classList.add('bod');
 
+// bod.addEventListener('mouseover', e => {
+//         bod.setAttribute('style','background-color: black');
+// });
 
 const btnBegin = document.createElement('button');
 btnBegin.textContent = 'Aperte para começar.';
 bod.appendChild(btnBegin);
 
+function reset(container) {
+    container.querySelectorAll('.grid-item')
+    container.forEach( e => e.parentNode.removeChild(e));
+}
+
 function createTable (number){
     const container = document.querySelector('.container');
+    //reset(container);
+    container.setAttribute('style','grid-template-columns: repeat('+ number +', 1fr)');
     for(i=1;i<=number*number;i++){
        let divsGrid = new Array();
        divsGrid[i] = document.createElement('div');
-       divsGrid[i].textContent = 'B'+i;
+       divsGrid[i].classList.add('divG');
        container.appendChild(divsGrid[i]);
+       //change the div color creating a track over the mouse 
+       divsGrid[i].addEventListener('mouseover', e => e.target.style.backgroundColor = 'blue');
     }
     return null;
 }
@@ -25,8 +37,11 @@ btnBegin.addEventListener('click',() => {
         numberOfDivs = prompt('Atenção. Digite um número de 1 a 100.')
     }
     createTable(numberOfDivs);
-    alert('Seu número é: '+numberOfDivs);
 });
+
+// divsGrid.addEventListener('onmouseover', () => {
+//     divsGrid.setAttribute('style','background-color: black');
+// });
 
 
  
